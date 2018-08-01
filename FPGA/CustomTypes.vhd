986f54d1463192816a7f152412c023644da60220
Filip Parag <filiparag.com@gmail.com>
2018-08-01 10:46:19 +0200

    Edited custom types

--- FPGA/CustomTypes.vhd
+++ FPGA/CustomTypes.vhd
@@ -16,8 +16,14 @@ package CustomTypes is
     type window_matrix      is array ((kernel_dimension * kernel_dimension - 1) downto 0) 	    of pixel;
     type window_matrix_bin  is array ((kernel_dimension * kernel_dimension - 1) downto 0) 	    of STD_LOGIC;
     type kernel_row         is array ((kernel_dimension - 1) downto 0) 	                        of pixel;
+
     type medminmax          is array (6 downto 0)                                               of pixel;
     type histogram          is array (255 downto 0) 	                                        of pixel;
-    type histogram_binary   is array (255 downto 0) 	                                        of STD_LOGIC;
+    type histogram_bin      is array (255 downto 0) 	                                        of STD_LOGIC;
+    type histogram_in       is array (224 downto 0)                                             of pixel;
+    type histogram_in_bin   is array (224 downto 0) 	                                        of STD_LOGIC;
+
+    type histogram_dec_out  is array (224 downto 0) 	                                        of histogram_bin;
+    type histogram_add_in   is array (255 downto 0) 	                                        of histogram_in_bin;
 
 end package CustomTypes;
