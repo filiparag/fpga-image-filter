31773028702788000e4c0647ca33bfedc0b47434
Mihailo Grbic <mihailogrbic99@gmail.com>
2018-08-02 22:04:38 +0200

    MinMedMax (bitonic_sort) implemented

--- FPGA/Bitonic/DelayRegister.vhd
+++ FPGA/Bitonic/DelayRegister.vhd
@@ -22,16 +22,18 @@ end DelayRegister;
 
 
 architecture delay_register of DelayRegister is
-    signal reg          : std_logic_vector (24 * N - 1 downto 0)
+	signal reg          : std_logic_vector (24 * N - 1 downto 0);
+	signal counter      : unsigned (4 downto 0)									:= (others => '0');
 begin
-    out_data <= reg (24 * (N - 1) + 23 downto 24 * (N - 1))
+	out_data <= reg (24 * (N - 1) + 23 downto 24 * (N - 1))
+	
 	process (
 	in_clk
 	)
 	begin
         if rising_edge(in_clk) and in_ready = '1' then
             for i in 0 to N - 2 loop
-                reg ((i + 1) * 24 + 23 downto (i + 1) * 24) <=  reg (i * 24 + 23 downto i * 24)
+                reg ((i + 1) * 24 + 23 downto (i + 1) * 24) <=  reg (i * 24 + 23 downto i * 24);
                 reg (23 downto 0) <= in_data;
             end loop;
 		end if;
