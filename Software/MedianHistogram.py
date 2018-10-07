bfe3de16439a9596a9388fe325f4fa680c33c888
Filip Parag <filiparag.com@gmail.com>
2018-10-07 21:50:09 +0200

    MedianHistogram implemented, doesn't pass the test

--- Software/MedianHistogram.py
+++ Software/MedianHistogram.py
@@ -2,119 +2,68 @@
 
 import numpy as np
 import converter as convert
+from PriorityDecoder import arch_priority_decoder
+from PriorityEncoder import arch_priority_encoder
+from HistogramAdder import arch_histogram_adder
 
-decoder_out = np.empty([225 * 256 * 8], dtype=np.uint8)
-adder_in = np.empty([256 * 225], dtype=np.uint8)
+decoder_out = np.empty([225 * 256], dtype=np.uint8)
+adder_in = np.empty([225 * 256], dtype=np.uint8)
 c_histogram = np.empty([256 * 8], dtype=np.uint8)
 pixel_enable = np.empty([225], dtype=np.uint8)
 
-write_dec = bool()
-write_add = bool()
-write_enc = bool()
+write_dec = bool(1)
+write_add = bool(1)
+write_enc = bool(1)
 
 def arch_median_histogram(gen_dimension, in_write, in_data):
 
-    out_median = int()
-    out_maximum = int()
-    out_minimum = int()
-    out_ready = bool()
+	out_median = int()
+	out_maximum = int()
+	out_minimum = int()
+	out_ready = bool()
 
-    global decoder_out
-    global adder_in
-    global c_histogram
-    global pixel_enable
+	global decoder_out
+	global adder_in
+	global c_histogram
+	global pixel_enable
 
-    global write_dec
-    global write_add
-    global write_enc
+	global write_dec
+	global write_add
+	global write_enc
 
-    for a in range(15):
-        for b in range(15):
-            if (b < (15 - gen_dimension) / 2 or b >= (15 - gen_dimension) / 2 + gen_dimension) or \
-               (a < (15 - gen_dimension) / 2 or a >= (15 - gen_dimension) / 2 + gen_dimension):
-                pixel_enable[15 * a + b] = '0'
-            if (b >= (15 - gen_dimension) / 2 and b < (15 - gen_dimension) / 2 + gen_dimension) and \
-               (a >= (15 - gen_dimension) / 2 and a < (15 - gen_dimension) / 2 + gen_dimension):
-                pixel_enable[15 * a + b] = '1'
+	for a in range(15):
+		for b in range(15):
+			if (b < (15 - gen_dimension) / 2 or b >= (15 - gen_dimension) / 2 + gen_dimension) or \
+			   (a < (15 - gen_dimension) / 2 or a >= (15 - gen_dimension) / 2 + gen_dimension):
+				pixel_enable[15 * a + b] = '0'
+			if (b >= (15 - gen_dimension) / 2 and b < (15 - gen_dimension) / 2 + gen_dimension) and \
+			   (a >= (15 - gen_dimension) / 2 and a < (15 - gen_dimension) / 2 + gen_dimension):
+				pixel_enable[15 * a + b] = '1'
 
-    # DEC_GEN : for p in 0 to kernel_dimension * kernel_dimension - 1 generate
-	# 	DEC : entity work.PriorityDecoder(priority_decoder)
-	# 	port map (
-	# 		in_clk 		=> in_clk,
-	# 		in_write	=> write_dec,
-	# 		in_value 	=> in_data(p),
-	# 		out_values 	=> decoder_out(p)
-	# 	);
-	# end generate;
+	for p in range(225):
+		decoder_out[256 * p : 256 * (p + 1)] = arch_priority_decoder(write_dec, in_data[8 * p : 8 * (p + 1)])
 
-    for d in range(225):
-        for a in range(256):
-            adder_in[a,d] = decoder_out[d,a]
+	for d in range(225):
+		for a in range(256):
+			adder_in[225 * a + d] = decoder_out[256 * d + a]
 
-    # ADD_GEN : for p in 0 to 255 generate
-	# 	ADD : entity work.HistogramAdder(histogram_adder)
-	# 	port map (
-	# 		in_clk 		=> in_clk,
-	# 		in_write	=> write_add,
-	# 		in_values 	=> adder_in(p),
-	# 		in_enabled 	=> pixel_enable,
-	# 		out_sum 	=> c_histogram(p)
-	# 	);
-	# end generate;
+	for p in range(256):
+		c_histogram[8 * p : 8 * (p + 1)] = arch_histogram_adder(write_add, adder_in[225 * p : 225 * (p + 1)], pixel_enable)
+	out_minimum = arch_priority_encoder('min', 15, write_enc, c_histogram)
+	out_median = arch_priority_encoder('med', 15, write_enc, c_histogram)
+	out_maximum = arch_priority_encoder('max', 15, write_enc, c_histogram)
 
-    # MIN : entity work.PriorityEncoder(priority_encoder)
-	# generic map
-	# ( 
-	# 	gen_lookup 		=> "min",
-	# 	gen_dimension	=> in_dimension
-	# )
-	# port map (
-	# 	in_clk 			=> in_clk,
-	# 	in_write		=> write_enc,
-	# 	in_histogram 	=> c_histogram,
-	# 	out_value 		=> out_minimum
-	# );
-
-	# MED : entity work.PriorityEncoder(priority_encoder)
-	# generic map
-	# ( 
-	# 	gen_lookup 		=> "med",
-	# 	gen_dimension	=> in_dimension
-	# )
-	# port map (
-	# 	in_clk 		 	=> in_clk,
-	# 	in_write	 	=> write_enc,
-	# 	in_histogram 	=> c_histogram,
-	# 	out_value 	 	=> out_median
-	# );
-
-	# MAX : entity work.PriorityEncoder(priority_encoder)
-	# generic map
-	# ( 
-	# 	gen_lookup 		=> "max",
-	# 	gen_dimension	=> in_dimension
-	# )
-	# port map (
-	# 	in_clk 			=> in_clk,
-	# 	in_write		=> write_enc,
-	# 	in_histogram 	=> c_histogram,
-	# 	out_value 		=> out_maximum
-	# );
-
-    write_dec = in_write
-    write_add = write_dec
-    write_enc = write_add
-    out_ready = write_enc
-
-    return out_median, out_maximum, out_minimum, out_ready
+	return out_minimum, out_median, out_maximum, True
 
 def test_median_histogram():
 
-    with open('median_histogram.in.test', 'r') as test_in, \
-         open('median_histogram.out', 'w') as test_out:
-        
-        in_data = convert.binstr_to_bin(test_in.readline().replace('\n', ''))
-        out_median, out_maximum, out_minimum, out_ready = arch_median_histogram(15, 1, in_data)
-        # test_out.write(convert.bin_to_binstr(out_values))
+	with open('median_histogram.in.test', 'r') as test_in, \
+		 open('median_histogram.out', 'w') as test_out:
+		
+		in_data = convert.binstr_to_bin(test_in.readline().replace('\n', ''))
+		out_minimum, out_median, out_maximum, out_ready = arch_median_histogram(15, 1, in_data)
+		test_out.write(convert.bin_to_binstr(out_minimum) + '\n')
+		test_out.write(convert.bin_to_binstr(out_median) + '\n')
+		test_out.write(convert.bin_to_binstr(out_maximum))
 
 test_median_histogram()
