e06b060e8cecc7a389cb373c6ab5092e456ef024
Filip Parag <filiparag.com@gmail.com>
2018-10-19 21:43:23 +0200

    Rewritten testbench for MinMedMax

--- Software/Test/MinMedMax/minmedmax_generate.py
+++ Software/Test/MinMedMax/minmedmax_generate.py
@@ -1,5 +1,6 @@
 #! /usr/bin/env python3
 
+from pprint import pprint
 import numpy as np
 from matplotlib import pyplot as plt
 
@@ -9,11 +10,12 @@ window = np.random.randint(256, size=(15, 15), dtype=np.uint8)
 
 def minmedmax(window, d=0):
 
-    if d == 0:
-        w = window.reshape(window.size)
-    else:
-        w = window[d:-d,d:-d].reshape(window[d:-d,d:-d].size)
-        # print(window[d:-d,d:-d])
+    # w = np.array([list(range(1,16))] * 15)
+
+    w = window[7 - d : 8 + d, 7 - d : 8 + d]
+    w = w.reshape(w.size)
+    # pprint(w)
+    # print(d, 2 * d + 1)
 
     min = int(np.min(w))
     med = int(np.median(w))
@@ -25,7 +27,7 @@ window_out = ''.join(list(map(lambda x: np.binary_repr(x, width=8), window.resha
 
 a_min, a_med, a_max = [], [], []
 
-for d in range(7):
+for d in range(1, 8):
         v_min, v_med, v_max = minmedmax(window, d)
         a_min.append(np.binary_repr(v_min, width=8))
         a_med.append(np.binary_repr(v_med, width=8))
@@ -38,6 +40,6 @@ with open('minmedmax_min.out.test', 'w') as f_min,\
      open('minmedmax_med.out.test', 'w') as f_med,\
      open('minmedmax_max.out.test', 'w') as f_max:
 
-    f_min.write('\n'.join(a_min))
-    f_med.write('\n'.join(a_med))
-    f_max.write('\n'.join(a_max))
+    f_min.write(''.join(a_min))
+    f_med.write(''.join(a_med))
+    f_max.write(''.join(a_max))
